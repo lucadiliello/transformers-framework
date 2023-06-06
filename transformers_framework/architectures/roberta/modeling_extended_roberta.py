ed8b3357df2e47d4b0848468c6071897142f053c
lucadiliello <luca.diliello@unitn.it>
2023-06-06 22:00:12 +0200

    removed match-case to keep compatibility with python3.9

--- transformers_framework/architectures/roberta/modeling_extended_roberta.py
+++ transformers_framework/architectures/roberta/modeling_extended_roberta.py
@@ -16,21 +16,20 @@ class RobertaExtendedClassificationHead(nn.Module):
         super().__init__()
         self.config = config
 
-        match (self.config.classification_head_type):
-            case 'IE_1' | 'IE_k' | 'AE_1':
-                self.classifier = ClassificationHead(
-                    config, hidden_size=config.hidden_size, num_labels=config.num_labels
-                )
-            case 'AE_k':
-                self.classifier = ClassificationHead(
-                    config, hidden_size=config.hidden_size * 2, num_labels=config.num_labels
-                )
-            case 'RE_k':
-                self.classifier = ClassificationHead(
-                    config, hidden_size=config.hidden_size, num_labels=config.k * config.num_labels
-                )
-            case _:
-                raise ValueError(f"Head type {self.config.classification_head_type} not among allowed")
+        if self.config.classification_head_type in ('IE_1', 'IE_k', 'AE_1'):
+            self.classifier = ClassificationHead(
+                config, hidden_size=config.hidden_size, num_labels=config.num_labels
+            )
+        elif self.config.classification_head_type in ('AE_k', ):
+            self.classifier = ClassificationHead(
+                config, hidden_size=config.hidden_size * 2, num_labels=config.num_labels
+            )
+        elif self.config.classification_head_type in ('RE_k', ):
+            self.classifier = ClassificationHead(
+                config, hidden_size=config.hidden_size, num_labels=config.k * config.num_labels
+            )
+        else:
+            raise ValueError(f"Head type {self.config.classification_head_type} not among allowed")
 
     def forward(self, hidden_state: torch.Tensor) -> torch.Tensor:
         r"""
@@ -43,42 +42,40 @@ class RobertaExtendedClassificationHead(nn.Module):
 
         seq_length = hidden_state.shape[1]
 
-        match (self.config.classification_head_type):
-
+        if self.config.classification_head_type == 'IE_1':
             # single label classification over the first CLS
-            case 'IE_1':
-                hidden_state = hidden_state[:, 0, :]
-                features = self.classifier(hidden_state)  # (batch_size, num_labels)
-            
+            hidden_state = hidden_state[:, 0, :]
+            features = self.classifier(hidden_state)  # (batch_size, num_labels)
+        
+        elif self.config.classification_head_type == 'IE_k':
             # multiple classifications over the first output token of the k sequences
-            case 'IE_k':
-                assert seq_length % (self.config.k + 1) == 0  # make sure it is divisible by k
-                seq_length = seq_length // (self.config.k + 1)
-                cls_positions = [seq_length * i for i in range(self.config.k + 1)]
-                hidden_state = hidden_state[:, cls_positions[1:], :]
-                features = self.classifier(hidden_state)  # (batch_size, k, num_labels)
+            assert seq_length % (self.config.k + 1) == 0  # make sure it is divisible by k
+            seq_length = seq_length // (self.config.k + 1)
+            cls_positions = [seq_length * i for i in range(self.config.k + 1)]
+            hidden_state = hidden_state[:, cls_positions[1:], :]
+            features = self.classifier(hidden_state)  # (batch_size, k, num_labels)
 
+        elif self.config.classification_head_type == 'AE_1':
             # single classification over the first output token of the k sequences
-            case 'AE_1':
-                assert seq_length % (self.config.k + 1) == 0  # make sure it is divisible by k
-                seq_length = seq_length // (self.config.k + 1)
-                cls_positions = [seq_length * i for i in range(self.config.k + 1)]
-                hidden_state = hidden_state[:, cls_positions[1:], :].sum(dim=1)
-                features = self.classifier(hidden_state)  # (batch_size, num_labels)
+            assert seq_length % (self.config.k + 1) == 0  # make sure it is divisible by k
+            seq_length = seq_length // (self.config.k + 1)
+            cls_positions = [seq_length * i for i in range(self.config.k + 1)]
+            hidden_state = hidden_state[:, cls_positions[1:], :].sum(dim=1)
+            features = self.classifier(hidden_state)  # (batch_size, num_labels)
 
+        elif self.config.classification_head_type == 'AE_k':
             # multiple classification on the concatenation of the first CLS with all the others
-            case 'AE_k':
-                hidden_state = torch.cat([
-                    hidden_state[:, cls_positions[0], :].repeat(1, self.config.k, 1),
-                    hidden_state[:, cls_positions[1:], :],
-                ], dim=-1)
-                features = self.classifier(hidden_state)  # (batch_size, k, num_labels)
+            hidden_state = torch.cat([
+                hidden_state[:, cls_positions[0], :].repeat(1, self.config.k, 1),
+                hidden_state[:, cls_positions[1:], :],
+            ], dim=-1)
+            features = self.classifier(hidden_state)  # (batch_size, k, num_labels)
 
+        elif self.config.classification_head_type == 'RE_k':
             # multiple classification all on the first CLS
-            case 'RE_k':
-                hidden_state = hidden_state[:, 0, :]
-                features = self.classifier(hidden_state)  # (batch_size, k * num_labels)
-                features = features.view(-1, self.config.k, self.config.num_labels)
+            hidden_state = hidden_state[:, 0, :]
+            features = self.classifier(hidden_state)  # (batch_size, k * num_labels)
+            features = features.view(-1, self.config.k, self.config.num_labels)
 
         return features
 
